aed6a151518001a8d51eeeec2c8e1fb493be9157
kusamao <kusamao0171@gmail.com>
2019-05-30 08:04:34 +0900

    4-3 done

--- bootpack.c
+++ bootpack.c
@@ -2,16 +2,20 @@
 /* 関数宣言しているので、naskfunc.nasの関数が実行できる */
 
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 
 void HariMain(void)
 {
 
     int i;
+    char *p;    /* BYTE[...]用の番地 */
+    //char * i;
 
     for (i = 0xa0000; i <= 0xaffff; i++) {
-        write_mem8(i, i & 0x0f); /* メモリに直接データを書き込む */
+        p = (char *) i;  /* 番地を代入 */
+        *p = i & 0x0f;  /* write_mem8(i, i & 0x0f);の代わりになる */
+        //*((char * ) i) = i & 0x0f;
+        //*i = (int) i & 0x0f;    /* 本とは違う実装方法を考えた */
     }
     
     /* 処理が終わったら無限HLT */
